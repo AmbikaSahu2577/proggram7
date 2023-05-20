289d1f95c7980886afb00c203bed545d27627039
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2023-05-20 17:02:35 +0530

    Add files via upload

---
+++ proggram7.java
@@ -0,0 +1,15 @@
+package com.exam;
+
+public class proggram7 {
+public static void main(String[] args) {
+	int arr[] = {10,20,30,40,50};
+	int max = 0;
+	for (int i = 0; i < arr.length; i++) {
+		if (arr[i]>max) 
+		{
+		   max=arr[i];	
+		}
+	}
+	System.out.println(max);
+}
+}
